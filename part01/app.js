be0ee32163e49a1bbaa1eb1edc670f6055c98154
Cepa95 <ceprnic.josip@gmail.com>
2023-09-29 10:32:04 +0200

    routing requests

--- part01/app.js
+++ part01/app.js
@@ -6,6 +6,16 @@ const http = require("http");
 // http.createServer(rqListener);
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title> Enter message </title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"> <input type="text"name="message"><button type="submit">send</button> </form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
   console.log(req.url, req.method, req.headers);
   // process.exit();
   res.setHeader("Content-Type", "text/html");
