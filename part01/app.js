87f4a82339faf2156240fc811fbefd9676385a86
Cepa95 <ceprnic.josip@gmail.com>
2023-09-29 10:22:03 +0200

    sending responses

--- part01/app.js
+++ part01/app.js
@@ -6,7 +6,14 @@ const http = require("http");
 // http.createServer(rqListener);
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  // process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title> My first page</title></head>");
+  res.write("<body> <h1> Hello from Node.js server </h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
