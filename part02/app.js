85ea49da93073b15dbb90451b473c80e2e8b3411
Cepa95 <ceprnic.josip@gmail.com>
2023-10-01 18:34:24 +0200

    handling different routes

--- part02/app.js
+++ part02/app.js
@@ -1,18 +1,30 @@
-const http = require("http");
+// const http = require("http");
 
 const express = require("express");
 
 const app = express();
 
-app.use((req,res, next) => {
-    console.log('In the middleware!')
-    next(); // da moze ici na sljedeci middleware
-})
-app.use((req,res, next) => {
-    console.log('In another middleware!')
-    res.send('<h1>Hello from Express</h1>');
-})
+// app.use((req,res, next) => {
+//     console.log('In the middleware!')
+//     next(); // da moze ici na sljedeci middleware
+// })
+app.use("/", (req, res, next) => {
+  console.log("this always runs twice");
+  next();
+});
 
-const server = http.createServer(app);
+app.use("/add-product", (req, res, next) => {
+  console.log("In another middleware");
+  res.send('<h1>the "add product" page</h1>');
+});
 
-server.listen(3000);
+app.use("/", (req, res, next) => {
+  console.log("In another middleware!");
+  res.send("<h1>Hello from Express</h1>");
+});
+
+app.listen(3000);
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
