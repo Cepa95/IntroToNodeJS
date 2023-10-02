5df2c17bf8c8f88f7e19a213fe1c9fa611e40bb4
Cepa95 <ceprnic.josip@gmail.com>
2023-10-02 12:31:31 +0200

    serving HTML pages

--- part02/routes/admin.js
+++ part02/routes/admin.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
@@ -5,11 +7,9 @@ const router = express.Router();
 
 // /admin/add-product
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>'
-  );
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
-//isti ptt moze biti koristen(add-product) ako su metode razlicite(GET,POST...).
+//isti http moze biti koristen(add-product) ako su metode razlicite(GET,POST...).
 router.post("/add-product", (req, res) => {
   console.log(req.body);
   res.redirect("/");
