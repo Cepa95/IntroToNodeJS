76c2938a386326c27290b69457dfd46b216d9c2b
Cepa95 <ceprnic.josip@gmail.com>
2023-10-02 12:50:54 +0200

    using helper function for nav

--- part02/routes/admin.js
+++ part02/routes/admin.js
@@ -2,12 +2,14 @@ const path = require("path");
 
 const express = require("express");
 
+const rootDir = require('../util/path')
+
 const router = express.Router();
 
 
 // /admin/add-product
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 //isti http moze biti koristen(add-product) ako su metode razlicite(GET,POST...).
 router.post("/add-product", (req, res) => {
