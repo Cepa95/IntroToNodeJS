e8c8ded1dc835142efd46d5507ecb0174faef8cf
Cepa95 <ceprnic.josip@gmail.com>
2023-10-05 11:34:39 +0200

    fetching from files

--- part02/models/product.js
+++ part02/models/product.js
@@ -1,13 +1,58 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
   save() {
-    products.push(this);
+    const p = path.join(__dirname, "..", "data", "products.json");
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        console.error(err);
+        return; 
+      }
+
+      let products = [];
+      if (fileContent.length > 0) {
+        try {
+          products = JSON.parse(fileContent);
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+          return; 
+        }
+      }
+
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    });
   }
-  static fetchAll() {
-    return products;
+
+  static fetchAll(cb) {
+    const p = path.join(__dirname, "..", "data", "products.json"); 
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        console.error(err);
+        cb([]); 
+        return;
+      }
+
+      let products = [];
+      if (fileContent.length > 0) {
+        try {
+          products = JSON.parse(fileContent);
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+          cb([]);
+          return;
+        }
+      }
+
+      cb(products);
+    });
   }
 };
