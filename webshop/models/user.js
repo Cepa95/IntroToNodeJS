e184758a16b5d6e8e759220410bde6a264e3284c
Cepa95 <ceprnic.josip@gmail.com>
2023-10-25 23:15:05 +0200

    storing the user in db

--- webshop/models/user.js
+++ webshop/models/user.js
@@ -1,16 +1,31 @@
-const Sequelize = require("sequelize");
+const getDb = require("../util/database").getDb;
+const mongodb = require("mongodb");
 
-const sequelize = require("../util/database");
+const ObjectId = mongodb.ObjectId;
 
-const User = sequelize.define("user", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING,
-});
+class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+  }
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .find({ _id: new ObjectId(userId) })
+      .toArray() // Convert cursor to an array
+      .then((users) => {
+        if (users.length === 0) {
+          return null; // User not found
+        }
+        return users[0]; // Return the first user found (assuming the ID is unique)
+      })
+      .catch((err) => console.error(err));
+  }
+}
 
 module.exports = User;
