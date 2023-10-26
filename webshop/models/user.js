6b7a23e5604d206ba6e08543ba4500630a5b044c
Cepa95 <ceprnic.josip@gmail.com>
2023-10-26 15:14:20 +0200

    displaying cart

--- webshop/models/user.js
+++ webshop/models/user.js
@@ -1,26 +1,40 @@
-const getDb = require("../util/database").getDb;
 const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
 const ObjectId = mongodb.ObjectId;
 
 class User {
   constructor(username, email, cart, id) {
-    this.username = username;
+    this.name = username;
     this.email = email;
     this.cart = cart;
     this._id = id;
   }
+
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
   }
 
   addToCart(product) {
-    // const cartProduct = this.cart.items.findIndex( cp => {
-    //   return cp._id === product._id;
-    // })
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
 
-    const updatedCart = { items: [{ productId: new ObjectId(product._id), quantity: 1 }] };
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity,
+      });
+    }
+    const updatedCart = {
+      items: updatedCartItems,
+    };
     const db = getDb();
     return db
       .collection("users")
@@ -28,6 +42,27 @@ class User {
         { _id: new ObjectId(this._id) },
         { $set: { cart: updatedCart } }
       );
+  }
+
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((i) => {
+      return i.productId;
+    });
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((p) => {
+          return {
+            ...p,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity,
+          };
+        });
+      });
   }
 
   static findById(userId) {
