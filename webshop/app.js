28e4592e0e5b38c98db49397296c1208a504258e
Cepa95 <ceprnic.josip@gmail.com>
2023-11-13 13:59:34 +0100

    sessions&cookies

--- webshop/app.js
+++ webshop/app.js
@@ -4,11 +4,22 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
+const MONGODB_URI =
+  "mongodb+srv://ceprnicjosip:" +
+  process.env.PASSWORD +
+  "@cluster0.liapsuw.mongodb.net/shop";
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -19,9 +30,17 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById('654383954508b7a84642aff2')
+  User.findById("654383954508b7a84642aff2")
     .then((user) => {
       req.user = user;
       next();
@@ -36,11 +55,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    "mongodb+srv://ceprnicjosip:" +
-      process.env.PASSWORD +
-      "@cluster0.liapsuw.mongodb.net/shop?retryWrites=true&w=majority"
-  )
+  .connect(MONGODB_URI)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
