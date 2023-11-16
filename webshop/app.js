2891ad9ee488c7bfde33d9e2bcc61ac2e856e09d
Cepa95 <ceprnic.josip@gmail.com>
2023-11-16 14:00:14 +0100

    deleting a cookie

--- webshop/app.js
+++ webshop/app.js
@@ -39,15 +39,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  User.findById("654383954508b7a84642aff2")
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
-});
-
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
