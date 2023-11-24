3a0f31b82c6ae1cd4600d0045e4b9e883f522a25
Cepa95 <ceprnic.josip@gmail.com>
2023-11-24 13:42:03 +0100

    auth flow

--- webshop/routes/admin.js
+++ webshop/routes/admin.js
@@ -2,25 +2,23 @@ const path = require("path");
 
 const express = require("express");
 
-// const rootDir = require("../util/path");
-
 const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
-///admin/add-product
+// /admin/add-product => GET
 router.get("/add-product", adminController.getAddProduct);
-//isti http moze biti koristen(add-product) ako su metode razlicite(GET,POST...).
+
+// /admin/products => GET
+router.get("/products", adminController.getProducts);
+
+// /admin/add-product => POST
 router.post("/add-product", adminController.postAddProduct);
-// //admin/products => get
-router.get('/products', adminController.getProducts);
 
-router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get("/edit-product/:productId", adminController.getEditProduct);
 
-router.post('/edit-product/', adminController.postEditProduct);
+router.post("/edit-product", adminController.postEditProduct);
 
-router.post('/delete-product', adminController.postDeleteProduct);
+router.post("/delete-product", adminController.postDeleteProduct);
 
 module.exports = router;
-// exports.routes = router;
-// exports.products = products;
